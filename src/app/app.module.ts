cae3985b4bbc2ce2b552cca5c44e29d2b2b3c83e
OatileM <osmdlela@gmail.com>
2023-11-12 19:00:36 +0200

    Resolved AWS Error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
